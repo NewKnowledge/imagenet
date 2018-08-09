278441a5998161bbcd2a75cbabe12633e0580a00
Craig Corcoran <craig@newknowledge.io>
2018-08-09 16:43:18 -0500

    add tensorflow install to environment file, turn cache off by default, add num obj env var, split out get_objects function, clean up some logging

--- cpu.dockerfile
+++ cpu.dockerfile
@@ -5,7 +5,8 @@ WORKDIR $HOME
 
 RUN apt-get update && apt-get install -y \
     curl \
-    build-essential 
+    build-essential \
+    git
 
 # install miniconda
 ENV PATH=$HOME/miniconda3/bin:$PATH 
@@ -17,15 +18,18 @@ RUN curl https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh >
 
 # install packages listed in environment.yml including cpu-optimized tensorflow
 COPY environment.yml $HOME/
-RUN conda env update -f /root/environment.yml && \
-    pip install --ignore-installed --no-deps --upgrade https://github.com/lakshayg/tensorflow-build/releases/download/tf1.9.0-ubuntu16.04-py36/tensorflow-1.9.0-cp36-cp36m-linux_x86_64.whl
+RUN conda env create /root/environment.yml
+ENV PATH=$HOME/miniconda3/envs/imagenet/bin:$PATH
+
+# RUN conda env update -f /root/environment.yml && \
+# pip install --ignore-installed --no-deps --upgrade https://github.com/lakshayg/tensorflow-build/releases/download/tf1.9.0-ubuntu16.04-py36/tensorflow-1.9.0-cp36-cp36m-linux_x86_64.whl
 
 # force dockerfile to download imagenet weights (.h5) into the image to avoid download on spin-up or first use
 # RUN python -c "from keras.applications.xception import Xception; Xception(weights='imagenet', include_top=False)"
-RUN python -c "from keras.applications.inception_v3 import InceptionV3; InceptionV3(weights='imagenet', include_top=False)"
-# RUN python -c "from keras.applications.mobilenetv2 import MobileNetV2; MobileNetV2(weights='imagenet', include_top=False)"
+# RUN python -c 'from keras.applications.inception_v3 import InceptionV3; InceptionV3()' 
+RUN python -c "from keras.applications.mobilenetv2 import MobileNetV2; MobileNetV2(weights='imagenet', include_top=False)" 
 
 COPY . $HOME/
-RUN pip install --no-deps -e .
+RUN pip install --no-deps -e . 
 
 CMD ["pytest", "--color=yes", "-s", "tests.py"]
--- nk_imagenet/imagenet.py
+++ nk_imagenet/imagenet.py
@@ -10,26 +10,33 @@ from keras.applications import inception_v3, mobilenetv2, xception
 
 from .utils import image_array_from_path, image_array_from_url, partition
 
+# NUM_OBJECTS = int(os.getenv('NUM_OBJECTS', '5'))
+
 logging.basicConfig(
     format="%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s",
     handlers=[
         logging.StreamHandler()
-        # logging.FileHandler(f"{logPath}/{fileName}.log"),
     ])
+# logging.FileHandler(f"{logPath}/{fileName}.log"),
 
 
 class ImagenetModel:
 
     ''' A class for featurizing images using pre-trained neural nets '''
 
-    def __init__(self, pooling=None, n_channels=None, cache_size=int(1e4), model='inception_v3', cache_dir='.'):
+    def __init__(self, include_top=False, pooling=None, n_channels=None, cache_size=int(1e4), model='inception_v3', cache_dir=None, n_objects=None):
 
+        self.include_top = include_top  # determines if used for classification or featurization, TODO separate into two classes?
         self.n_channels = n_channels
+        self.n_objects = n_objects
+
         self.failed_urls = set()
+
         # NOTE: set cache_dir to None to turn off caching
         if cache_dir:
             # create default cache path in the current file dir w/ filename specifying config
-            config = [str(cache_size), model, str(pooling) if pooling else '', str(n_channels) if n_channels else '']
+            config = [f'objects-{NUM_OBJECTS}' if include_top else 'features', str(cache_size), model,
+                      pooling if pooling else '', str(n_channels) if n_channels else '']
             config_str = '-'.join([c for c in config if c])  # filter out empty strings and join w/ -
             cache_fname = f'imagenet-cache-{config_str}.pkl'
             self.cache_path = os.path.join(cache_dir, cache_fname)
@@ -43,23 +50,29 @@ class ImagenetModel:
             self.cache = LRUCache(cache_size)
 
         if model == 'xception':
-            self.model = xception.Xception(weights='imagenet', include_top=False, pooling=pooling)
+            self.model = xception.Xception(weights='imagenet', include_top=include_top, pooling=pooling)
             self.preprocess = xception.preprocess_input
-            self.decode = xception.decode_predictions
             self.target_size = (299, 299)
-            self.output_dim = (n_channels if n_channels else 2048) * (1 if pooling else 10**2)
+            if include_top:
+                self.decode = xception.decode_predictions
+            else:
+                self.output_dim = (n_channels if n_channels else 2048) * (1 if pooling else 10**2)
         elif model == 'inception_v3':
-            self.model = inception_v3.InceptionV3(weights='imagenet', include_top=False, pooling=pooling)
+            self.model = inception_v3.InceptionV3(weights='imagenet', include_top=include_top, pooling=pooling)
             self.preprocess = inception_v3.preprocess_input
-            self.decode = inception_v3.decode_predictions
             self.target_size = (299, 299)
-            self.output_dim = (n_channels if n_channels else 2048) * (1 if pooling else 8**2)
+            if include_top:
+                self.decode = inception_v3.decode_predictions
+            else:
+                self.output_dim = (n_channels if n_channels else 2048) * (1 if pooling else 8**2)
         elif model == 'mobilenet_v2':
-            self.model = mobilenetv2.MobileNetV2(weights='imagenet', include_top=False, pooling=pooling)
+            self.model = mobilenetv2.MobileNetV2(weights='imagenet', include_top=include_top, pooling=pooling)
             self.preprocess = mobilenetv2.preprocess_input
-            self.decode = mobilenetv2.decode_predictions
             self.target_size = (244, 244)
-            self.output_dim = (n_channels if n_channels else 1280) * (1 if pooling else 7**2)
+            if include_top:
+                self.decode = mobilenetv2.decode_predictions
+            else:
+                self.output_dim = (n_channels if n_channels else 1280) * (1 if pooling else 7**2)
         else:
             raise Exception('model option not implemented')
 
@@ -89,34 +102,98 @@ class ImagenetModel:
             logging.info(f'successfully loaded cache with {len(self.cache)} entries \
                          and failed urls with {len(self.failed_urls)} entries')
 
+    def get_objects_from_url(self, image_url, ignore_failed=True):
+        ''' detects objects from image in a url, returns None if url download failed '''
+        if image_url not in self.cache:
+            # skip if we're ignoring previously failed urls
+            if ignore_failed and image_url in self.failed_urls:
+                return
+
+            # download image and convert into numpy array
+            image_array = image_array_from_url(image_url, target_size=self.target_size)
+            if image_array is None:
+                # if url request failed, add to failed set
+                self.failed_urls.add(image_url)
+                return
+
+            # add a dim if needed
+            if image_array.ndim == 3:
+                image_array = image_array[None, :, :, :]
+
+            # use the imagenet model to detect the objects in the image and add result to cache
+            self.cache[image_url] = self.get_objects(image_array)
+
+        # returned cached result
+        return self.cache[image_url]
+
+    def get_objects(self, image_array):
+        ''' detects objects in image provided as an array '''
+        logging.debug(f'recognizing objects')
+        image_array = self.preprocess(image_array)
+        objects = self.model.predict(image_array)
+        objects = self.decode(objects, top=self.n_objects)[0]
+        return {obj[1]: obj[2] for obj in objects}  # objects = [{'object': obj[1], 'score': obj[2]} for obj in objects]
+
     def get_features_from_paths(self, image_paths):
         ''' takes a list of image filepaths and returns the features resulting from applying the imagenet model to those images '''
+        if self.include_top:
+            raise Exception('getting features from a classification model with include_top=True is currently not supported')
         # TODO add caching for paths like urls
         images_array = np.array((image_array_from_path(fpath, target_size=self.target_size) for fpath in image_paths))
         return self.get_features(images_array)
 
     def get_features_from_url(self, image_url):
         ''' attempt to download the image at the given url, then return the imagenet features if successful, and None if not '''
-        if image_url in self.cache:
-            return self.cache[image_url]
-        else:
+        if self.include_top:
+            raise Exception('getting features from a classification model with include_top=True is currently not supported')
+
+        if image_url not in self.cache:
             image_array = image_array_from_url(image_url, target_size=self.target_size)
-            if image_array is not None:
+            if image_array is None:
+                self.failed_urls.add(image_url)
+                return
+            else:
                 if image_array.ndim == 3:
                     image_array = image_array[None, :, :, :]
-                return self.get_features(image_array)
+                self.cache[image_url] = self.get_features(image_array)
+
+        return self.cache.get(image_url)
 
     def get_features_from_url_batch(self, image_urls, ignore_failed=True):
         ''' takes a list of image urls and returns the features resulting from applying the imagenet model to
         successfully downloaded images along with the urls that were successful. Cached values are used when available
         '''
+        if self.include_top:
+            raise Exception('getting features from a classification model with include_top=True is currently not supported')
         # split urls into new ones and ones that have cached results
-        new_urls, cached_urls = partition(lambda x: x in self.cache, image_urls, as_list=True)
+        new_urls = image_urls
+        cached_urls = []
+        # new_urls, cached_urls = partition(lambda x: x in self.cache, image_urls, as_list=True)
         logging.info(f'getting image arrays from {len(image_urls)} urls; \
                      {len(new_urls)} new urls and {len(cached_urls)} cached urls')
         if cached_urls:
             logging.debug(f'loading features for {len(cached_urls)} images from cache')
-            cached_image_features = np.array([self.cache[url] for url in cached_urls])
+            if len(cached_urls) == 1:
+                cached_image_features = self.cache[cached_urls[0]]
+                # print('pre cached dim:', cached_image_features.ndim)
+                # if cached_image_features.ndim == 1:
+                #     cached_image_features = cached_image_features[None, :]
+                # elif cached_image_features.ndim == 3:
+                #     assert cached_image_features.shape[:1] == (1, 1)
+                #     cached_image_features = cached_image_features[0, :, :]
+                # print('post cached dim:', cached_image_features.ndim)
+                assert cached_image_features.ndim == 2
+            else:
+                cached_image_features = np.array([self.cache[url] for url in cached_urls])
+                # print('pre cached dim:', cached_image_features.ndim)
+                # if cached_image_features.ndim == 1:
+                #     cached_image_features = cached_image_features[None, :]
+                # elif cached_image_features.ndim == 3:
+                #     assert cached_image_features.shape[:1] == (1, 1)
+                #     cached_image_features = cached_image_features[0, :, :]
+                # print('cached dim:', cached_image_features.ndim)
+                assert cached_image_features.ndim == 2
+            # print('cached dim:', cached_image_features.ndim)
 
         # remove new urls known to fail
         if new_urls and ignore_failed:
@@ -147,12 +224,16 @@ class ImagenetModel:
 
                 logging.debug(f'getting features from image arrays with shape {new_image_arrays.shape}')
                 new_image_features = self.get_features(new_image_arrays)
+                assert new_image_features.ndim == 2
                 logging.debug(f'got features array with shape {new_image_features.shape}')
                 # add new image features to cache
                 logging.info('saving features to cache')
+
                 self.cache.update(zip(new_urls, new_image_features))
 
         if cached_urls and new_urls and downloaded_images:
+            # print('cached:', cached_image_features.shape)
+            # print('new: ', new_image_features.shape)
             logging.debug('cached and new')
             # combine results
             image_features = np.vstack((cached_image_features, new_image_features))
@@ -173,15 +254,21 @@ class ImagenetModel:
 
     def get_features(self, images_array):
         ''' takes a batch of images as a 4-d array and returns the (flattened) imagenet features for those images as a 2-d array '''
+        if self.include_top:
+            raise Exception('getting features from a classification model with include_top=True is currently not supported')
+
         if images_array.ndim != 4:
             raise Exception('invalid input shape for images_array, expects a 4d array')
+
+        # preprocess and compute image features
         logging.debug(f'preprocessing {images_array.shape[0]} images')
         images_array = self.preprocess(images_array)
         logging.debug(f'computing image features')
         image_features = self.model.predict(images_array)
+
+        # if n_channels is specified, only keep that number of channels
         if self.n_channels:
             logging.debug(f'truncating to first {self.n_channels} channels')
-            # if n_channels is specified, only keep that number of channels
             image_features = image_features.T[: self.n_channels].T
 
         # reshape output array by flattening each image into a vector of features
