ee53b6357465c19140fd18172a1eb7aa008986da
jgleason <jeffrey.gleason@newknowledge.io>
2019-08-13 10:55:44 -0500

    add finetune and finetuned_predict methods to imagenet cls

--- nk_imagenet/imagenet.py
+++ nk_imagenet/imagenet.py
@@ -7,6 +7,9 @@ import pickle
 import numpy as np
 from cachetools import LRUCache
 from keras.applications import inception_v3, mobilenetv2, xception
+from keras.models import Model
+from keras.layers import Dense, GlobalAveragePooling2D, GlobalMaxPooling2D
+from keras.optimizers import SGD
 
 from .utils import image_array_from_path, image_array_from_url, partition
 
@@ -29,6 +32,7 @@ class ImagenetModel:
         self.include_top = include_top  # determines if used for classification or featurization, TODO separate into two classes?
         self.n_channels = n_channels
         self.n_objects = n_objects
+        self.pooling = pooling
 
         self.failed_urls = set()
 
@@ -278,3 +282,71 @@ class ImagenetModel:
     def predict(self, images_array):
         ''' alias for get_features to more closely match scikit-learn interface '''
         return self.get_features(images_array)
+
+    def finetune(self, 
+                images_array, 
+                labels, 
+                pooling = 'avg',
+                nclasses = 2,
+                batch_size = 32, 
+                top_layer_epochs = 1,
+                frozen_layer_count = 249,
+                all_layer_epochs = 5, 
+                class_weight = None,
+                optimizer = 'rmsprop'
+        ):
+        ''' Finetunes the Imagenet model iteratively on a smaller set of images with (potentially) a smaller set of classes.
+            First finetunes last layer then freezes bottom N layers and retrains the rest
+        '''
+
+        if images_array.ndim != 4:
+            raise Exception('invalid input shape for images_array, expects a 4d array')
+        
+        # preprocess images
+        logging.debug(f'preprocessing {images_array.shape[0]} images')
+        images_array = self.preprocess(images_array)
+
+        # create new model for finetuned classification
+        out = self.model.output
+        if self.pooling is None:
+            out = GlobalAveragePooling2D()(out) if pooling == 'avg' else GlobalMaxPooling2D()(out)
+        dense = Dense(1024, activation='relu')(out)
+        preds = Dense(nclasses, activation='softmax')(dense)
+        self.finetune_model = Model(inputs = self.model.input, outputs = preds)
+
+        # freeze all convolutional InceptionV3 layers, retrain top layer
+        for layer in self.finetune_model.layers:
+            layer.trainable = False
+        self.finetune_model.compile(optimizer=optimizer, loss='categorical_crossentropy')
+        self.finetune_model.fit(images_array, 
+            np.array(labels), 
+            batch_size = batch_size,
+            epochs = top_layer_epochs,
+            class_weight = class_weight)
+
+        # freeze bottom N convolutional layers, retrain top M-N layers (M = total number of layers)
+        for layer in self.finetune_model.layers[:frozen_layer_count]:
+            layer.trainable = False
+        for layer in self.finetune_model.layers[frozen_layer_count:]:
+            layer.trainable = True
+
+        # use SGD and low learning rate to prevent catastrophic forgetting in these blocks
+        self.finetune_model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='categorical_crossentropy')
+        self.finetune_model.fit(images_array, 
+            np.array(labels), 
+            batch_size = batch_size,
+            epochs = all_layer_epochs,
+            class_weight = class_weight)
+
+    def finetuned_predict(self, images_array):
+        ''' Uses the finetuned model to predict on an image array. Returns array of softmax prediction probabilities 
+        '''    
+
+        if images_array.ndim != 4:
+            raise Exception('invalid input shape for images_array, expects a 4d array')
+
+        # preprocess image array
+        logging.debug(f'preprocessing {images_array.shape[0]} images')
+        images_array = self.preprocess(images_array)
+
+        return self.finetune_model.predict(images_array)
