04d250f4a4e0adb412f41745395038a9b2d84f9a
Geoffrey Gross <geoffreyrgross@gmail.com>
2019-11-19 16:45:52 -0600

    Take out tensorflow-gpu and bump imagenet versino

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 from setuptools import setup
 
 
-setup(name='nk_imagenet',
-      version='1.0.0',
-      description='Interface for using pretrained imagenet models to generate features or object predictions',
-      packages=['nk_imagenet'],
-      include_package_data=True,
-      install_requires=[
-          'cachetools >= 2.1.0',
-          'Keras == 2.2.4',
-          'numpy == 1.15.4',
-          'Pillow >= 5.1.0',
-          'pytest >= 3.6.2',
-          'requests == 2.19.1',
-          'tensorflow-gpu <= 1.12.2',
-      ])
+setup(
+    name="nk_imagenet",
+    version="1.1.0",
+    description="Interface for using pretrained imagenet models to generate features or object predictions",
+    packages=["nk_imagenet"],
+    include_package_data=True,
+    install_requires=[
+        "cachetools >= 2.1.0",
+        "Keras == 2.2.4",
+        "numpy == 1.15.4",
+        "Pillow >= 5.1.0",
+        "pytest >= 3.6.2",
+        "requests == 2.19.1",
+    ],
+)
